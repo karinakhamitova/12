d37d3550198f9db0289fea415526d36383362f14
TopFps <TopFps@DESKTOP-UUEU6H0>
2022-11-07 23:57:52 +0300

    Добавьте файлы проекта.

---
+++ 12_1/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_1
+{
+    static class Circle
+    {
+        public static double GetLength(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+        public static double GetArea(double r)
+        {
+            return Math.PI * r * r;
+        }
+        public static string CheckСoordinates(double r, double x0, double y0, double x, double y)
+        {
+            if (Math.Sqrt(Math.Pow(x -x0,2)+ Math.Pow(y - y0, 2))>r)
+            {
+                return "Точка выходит за границы круга";
+            }
+            return "Точка находится внутри круга";
+        }
+    }
+}
---
+++ 12_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Для определения параметров круга введите радиус:");
+            double r = Convert.ToDouble(Console.ReadLine());
+            double p = Circle.GetLength(r);
+            double s = Circle.GetArea(r);
+            Console.WriteLine($"Длина окружности - {p}, площадь круга - {s}");
+            Console.WriteLine("Поверка принадлежности точки с координатами (x,y) кругу с радиусом r и координатами центра x0, y0. Введите значения координат центра и точки:  ");
+            double x0 = Convert.ToDouble(Console.ReadLine());
+            double y0 = Convert.ToDouble(Console.ReadLine());
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Circle.CheckСoordinates(r, x0, y0, x, y));
+            Console.ReadKey();
+        }
+    }
+}
